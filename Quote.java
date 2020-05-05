503064d734497b9f8c80c232b46707b0b33f96fe
Vladislav Kryshtanovskiy <wladislav.k@gmail.com>
2020-05-04 19:35:53 -0700

    Added javadoc

--- Quote.java
+++ Quote.java
@@ -1,17 +1,38 @@
 import java.text.*;
 import java.util.List;
 
+/**
+ * Retrieval of quotes based on rate data
+ */
 public class Quote {
     private List<VehicleRate> vehicleRates;
 
+    /**
+     * @param vehicleRates Data on available vehicles
+     */
     public Quote(List<VehicleRate> vehicleRates) {
         this.vehicleRates = vehicleRates;
     }
 
+    /**
+     * Calculate vehicle rate in whole cents
+     *
+     * @param vehicleRate Data on currently selected vehicle rate
+     * @param vehicleCount Number of vehicles needed
+     * @param numberOfUnits Number of pricing units
+     * @return Rate in whole cents
+     */
     private int getRateInCents(VehicleRate vehicleRate, int vehicleCount, int numberOfUnits) {
         return vehicleCount * vehicleRate.getRateInCents() * numberOfUnits;
     }
 
+    /**
+     * Choose which rate is needed
+     *
+     * @param vehicleType Type of vehicle
+     * @param pricingMethod Type of pricing
+     * @return Data on needed vehicle rate
+     */
     private VehicleRate getCurrentVehicleRate(String vehicleType, String pricingMethod) {
         for (VehicleRate vehicleRate: vehicleRates) {
             if (vehicleType.equals(vehicleRate.getVehicleType()) && pricingMethod.equals(vehicleRate.getPricingMethod())) {
@@ -21,6 +42,14 @@ public class Quote {
         throw new RuntimeException("Quote cannot be found in the data set");
     }
 
+    /**
+     *
+     * @param vehicleCount Number of vehicles needed
+     * @param vehicleType Type of vehicle
+     * @param pricingMethod Type of pricing
+     * @param numberOfUnits Number of pricing units
+     * @return Formatted rate: $#.##
+     */
     public String getRate(int vehicleCount, String vehicleType, String pricingMethod, int numberOfUnits) {
         VehicleRate currentVehicleRate = getCurrentVehicleRate(vehicleType, pricingMethod);
 
